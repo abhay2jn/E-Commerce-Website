92fb96784c8a99516797e30ad39812b01554c272
abhay <abhayforwork02@gmail.com>
2024-05-28 17:36:27 +0530

    issues fixed

--- src/App.js
+++ src/App.js
@@ -1,61 +1,79 @@
 import { useState } from "react";
-import Navigation from  "./Nav-bar/Nav"
+
+import Navigation from "./Nav-bar/Nav"
 import Products from "./Products/Products";
+import products from "./All-data/data";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
-import products from "./All-data/data";
 import Card from "./components/Card";
-import "./index.css"
+import "./index.css";
 
 function App() {
-  const [selectedCategory,setSelectedCategory] = useState(null);
-  const [query,setQuery] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  // ----------- Input Filter -----------
+  const [query, setQuery] = useState("");
 
   const handleInputChange = (event) => {
     setQuery(event.target.value);
-  }
+  };
 
-  const filteredItems = products.filter((product) => 
-  product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
 
+  // ----------- Radio Filtering -----------
   const handleChange = (event) => {
     setSelectedCategory(event.target.value);
-  }
+  };
 
+  // ------------ Button Filtering -----------
   const handleClick = (event) => {
     setSelectedCategory(event.target.value);
-  }
+  };
 
-  function filteredData(products,selected,query) {
+  function filteredData(products, selected, query) {
     let filteredProducts = products;
-    if(query) {
+
+    // Filtering Input Items
+    if (query) {
       filteredProducts = filteredItems;
     }
+
+    // Applying selected filter
     if (selected) {
-      filteredProducts = filteredProducts.filter(({ category , color , company,newPrice,title}) => 
-        category === selected || color === selected || company === selected || title === selected || newPrice === selected
-    );
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
     }
-    return filteredProducts.map(({img,title,star,reviews,prevPrice,newPrice}) => (
-      <Card key={Math.random()} img={img}
-        title = {title}
-        star = {star}
-        reviews = {reviews}
-        newPrice = {newPrice}
-        prevPrice = {prevPrice}
-      />
-    )
-  );
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
   }
 
-  const result  = filteredData(products,query,selectedCategory);
-
+  const result = filteredData(products, selectedCategory, query);
 
   return (
     <>
-    <Sidebar handleChange = {handleChange} />
+      <Sidebar handleChange={handleChange} />
       <Navigation query={query} handleInputChange={handleInputChange} />
-      <Recommended handleClick={handleClick}/>
+      <Recommended handleClick={handleClick} />
       <Products result={result} />
     </>
   );
